fe8696ad71e52302557eb95a8e9d5b1e4df79346
Diego Bassan <bsn.diego@gmail.com>
2019-03-23 15:46:03 -0300

    Firt Projects Commit.
    Createt first test.

---
+++ TestsDotNet/Hero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsDotNet
+{
+    public class Hero
+    {
+        public string Name { get; set; }
+        public string HeroClass { get; set; }
+
+        public double Life { get; set; }
+
+        public int Level { get; set; }
+    }
+}
